492764bcef2037066bb9c93f9aa239740be832bf
Jason Jones <jasonedwardjones@gmail.com>
2018-04-09 22:41:58 -0800

    wip

--- migrations/1523340620_create_table_cell.up.sql
+++ migrations/1523340620_create_table_cell.up.sql
@@ -5,5 +5,6 @@ create table cell
     name character varying, 
     the_geog geography(POINT,4326) not null,
     constraint cell_pkey primary key (cell_id)
-    constraint fk_layer foreign key(layer_id) references layer(layer_id)
 );
+
+alter table cell add constraint fk_cell_layer foreign key(layer_id) references layer(layer_id);
